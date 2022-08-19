3a9b8dc9671a413aec3d43b75528ac6eb74c1096
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-19 18:00:58 -0300

    chore(babel): configurado o babel

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {targets: { node: 'current' }}],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          "@config": ["./src/config"],
+          "@errors": ["./src/shared/errors"],
+          "@middlewares": ["./src/shared/infra/http/middlewares"],
+        }
+      }
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", {legacy: true}],
+    ["@babel/plugin-proposal-class-properties", {loose: true}],
+		["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+  ]
+}
